65e3e78ba9f82c8f5b6a1ff76eecb0cc0a2ce3ed
Anupam Sharma <anupam.attrey@hotmail.com>
2024-01-10 09:09:08 -0500

    Adding Greeting endpoint point

--- src/main/java/com/anupam/controller/CustomerController.java
+++ src/main/java/com/anupam/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package com.anupam.controller;
 
+import com.anupam.model.Greeting;
 import com.anupam.model.response.Response;
 import com.anupam.service.CustomerService;
 import org.slf4j.Logger;
@@ -8,7 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class CustomerController {
@@ -16,12 +20,17 @@ public class CustomerController {
     @Autowired
     CustomerService customerService;
 
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
     @GetMapping("/customer/{id}")
     public ResponseEntity<Response> customerInfo(@PathVariable final String id){
-        LOG.info("====================== {} ",id);
-
-
+        LOG.info("Calling CustomerController with customer Id {} ",id);
         Response response = null;
         return ResponseEntity.ok(customerService.getCustomerById(id));
+    }
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
---
+++ src/main/java/com/anupam/model/Greeting.java
@@ -0,0 +1,3 @@
+package com.anupam.model;
+
+public record Greeting(long id, String content) { }
