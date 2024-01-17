755cf26e054549ccc7d4ff498211358968d2f33f
Anupam Sharma <anupam.attrey@hotmail.com>
2024-01-16 21:22:33 -0500

    Adding AWS Secret key and Id while deploying

--- src/main/java/com/anupam/config/AWSConfiguration.java
+++ src/main/java/com/anupam/config/AWSConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
@@ -21,54 +22,13 @@ public class AWSConfiguration {
     @Value("${aws.region}")
     private String region;
 
-    private static GetSecretValueResponse getSecret(SecretsManagerClient client, String secretName) {
-
-        GetSecretValueRequest request = GetSecretValueRequest.builder()
-                .secretId("anupam/secret/")
-                .build();
-        return client.getSecretValue(request);
-    }
-
     public DynamoDbClient createDBClient() {
-        String accessKeySecretName = "key1";
-        String secretKeySecretName = "key2";
 
-        SecretsManagerClient secretsClient = SecretsManagerClient.builder()
-                .region(Region.of("us-east-1"))
+        DynamoDbClient client = DynamoDbClient.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .build();
-        GetSecretValueResponse accessKeyResponse = getSecret(secretsClient, accessKeySecretName);
-        String accessKey = accessKeyResponse.secretString();
 
-        GetSecretValueResponse secretKeyResponse = getSecret(secretsClient, secretKeySecretName);
-        String secretKey = secretKeyResponse.secretString();
-
-
-        /**
-         * Create the AWS credentials provider with BasicAWSCredentials
-         */
-        AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey,
-                secretKey));
-
-        /**
-         * Adding this code only for local testing for timeout use cases for AWS DynamoDB
-         * Set up a custom SdkHttpClient with a connection timeout (in milliseconds)
-         **/
-
-        /**
-         * int connectionTimeoutMillis = 1; // 1 ms
-         * SdkHttpClient httpClient = ApacheHttpClient.builder().connectionTimeout(Duration.ofMillis(connectionTimeoutMillis)).build();
-         */
-
-        /**
-         * Create DynamoDB client with the AWS credentials
-         */
-        return DynamoDbClient.builder()
-                .region(Region.of(region))
-                /**
-                 * Un-Comment below line , if want to test time-out use case
-                 */
-                //.httpClient(httpClient)
-                .credentialsProvider(credentialsProvider)
-                .build();
+       return client;
     }
 }
