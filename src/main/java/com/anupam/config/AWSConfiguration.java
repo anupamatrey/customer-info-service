f04d9eb3372652060215553a2865dbf42e093715
Anupam Sharma <anupam.attrey@hotmail.com>
2024-01-19 13:04:02 -0500

    Adding AWS Secret key and Id while deploying

--- src/main/java/com/anupam/config/AWSConfiguration.java
+++ src/main/java/com/anupam/config/AWSConfiguration.java
@@ -1,7 +1,10 @@
 package com.anupam.config;
 
 
+import com.anupam.controller.CustomerController;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -15,14 +18,20 @@ import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 
+import java.util.Map;
+
 
 @Configuration
 @Slf4j
 public class AWSConfiguration {
+    private final static Logger LOG = LoggerFactory.getLogger(AWSConfiguration.class);
     @Value("${aws.region}")
     private String region;
 
     public DynamoDbClient createDBClient() {
+        Map<String,String> env = System.getenv();
+        env.forEach((k,v)-> System.out.println(k+" : "+v));
+
         /**
          * Create the AWS credentials provider with BasicAWSCredentials
          */
